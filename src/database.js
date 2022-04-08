4f9c6d034dcae6536e7b8aa32442519c989504bd
jdnovar <jdnovar@unal.edu.co>
2022-04-08 16:49:00 -0400

    Commit2

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //URL A LA BASE DE DATOS
-//const URI = 'mongodb+srv://jaiber:nintendo64@freecluster.2o9li.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const URL = 'mongodb+srv://root:ferremax2021@ferremax.miudj.mongodb.net/Ferremax?retryWrites=true&w=majority';
 
 //MENSAJE DE CONEXION EXITOSA
